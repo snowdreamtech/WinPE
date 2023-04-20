510cca1b2d1431550c18ae292ca85b5e0c549153
sn0wdr1am <sn0wdr1am@qq.com>
2023-04-20 17:55:31 +0800

    Fix： Startnet.bat

--- Win10/Startnet.bat
+++ Win10/Startnet.bat
@@ -4,9 +4,9 @@ echo powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 )>"C:\WinPE_!archdir!\mount\windows\system32\startnet.cmd" 
 
 if !arch! EQU x64 (
-echo "explorer++ %SYSTEMDRIVE%\Program Files">>>"C:\WinPE_!archdir!\mount\windows\system32\startnet.cmd" 
+echo "%%SYSTEMDRIVE%%\Program Files\Explorer++\explorer++" "%%SYSTEMDRIVE%%\Program Files" >> "C:\WinPE_!archdir!\mount\windows\system32\startnet.cmd" 
 ) 
 
 if !arch! EQU x86 (
-echo "explorer++ %SYSTEMDRIVE%\Program Files (x86)">>>"C:\WinPE_!archdir!\mount\windows\system32\startnet.cmd" 
+echo "%%SYSTEMDRIVE%%\Program Files (x86)\Explorer++\explorer++" "%%SYSTEMDRIVE%%\Program Files (x86)" >> "C:\WinPE_!archdir!\mount\windows\system32\startnet.cmd" 
 )
