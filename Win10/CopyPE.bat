ccfaff8505fca18ccef7c4dcb3e575f64624c7c0
sn0wdr1am <sn0wdr1am@qq.com>
2023-04-22 12:43:54 +0800

    Delet: unused comment

--- Win10/CopyPE.bat
+++ Win10/CopyPE.bat
@@ -1,5 +1,4 @@
 call "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
 
-:: 创建 Windows PE 文件的工作副本：
 if exist "C:\WinPE_!archdir!" rd /s/q "C:\WinPE_!archdir!" 
 copype !archdir! C:\WinPE_!archdir!
